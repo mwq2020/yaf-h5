4b542990a83b3cd1300b8b3c6ea77cc23f22a1d3
mwq2020 <mwq2020@163.com>
2018-02-28 16:35:09 +0800

    fix

--- application/library/Wechat/Wxcrypt.php
+++ application/library/Wechat/Wxcrypt.php
@@ -9,7 +9,7 @@ class Wxcrypt
      */
     public function decodeCryptData($appid,$sessionKey,$encryptedData,$iv)
     {
-        include_once './wxcrypt/wxBizDataCrypt.php';
+        include_once __DIR__'/wxcrypt/wxBizDataCrypt.php';
         $pc = new WXBizDataCrypt($appid, $sessionKey);
         $errCode = $pc->decryptData($encryptedData, $iv, $data);
         if ($errCode == 0) {
