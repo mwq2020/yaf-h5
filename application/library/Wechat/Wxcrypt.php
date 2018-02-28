c98fadc26c0242d9403531e42eb5e4e638ba7fee
mwq2020 <mwq2020@163.com>
2018-02-28 16:31:53 +0800

    fix

---
+++ application/library/Wechat/Wxcrypt.php
@@ -0,0 +1,22 @@
+<?php
+namespace Wechat;
+
+class Wxcrypt 
+{
+
+    /**
+     * 解密微信运动数据.
+     */
+    public function decodeCryptData($appid,$sessionKey,$encryptedData,$iv)
+    {
+        include_once './wxcrypt/wxBizDataCrypt.php';
+        $pc = new WXBizDataCrypt($appid, $sessionKey);
+        $errCode = $pc->decryptData($encryptedData, $iv, $data);
+        if ($errCode == 0) {
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+}
