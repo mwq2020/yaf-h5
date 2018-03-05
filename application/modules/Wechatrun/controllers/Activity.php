31e3d852d67ffaa7ca6e2b2478323c9386a8461a
mwq2020 <mwq2020@163.com>
2018-03-05 14:25:12 +0800

    fix

--- application/modules/Wechatrun/controllers/Activity.php
+++ application/modules/Wechatrun/controllers/Activity.php
@@ -83,6 +83,8 @@ class ActivityController extends Core\Base
                     )  
                 ->groupBy('w_company_user.user_id')
                 ->where(['w_company_user.company_id' => $company_id])
+                ->where('w_step_log.data_time','>=',strtotime('2018-03-01'))
+                ->where('w_step_log.data_time','<=',strtotime('2018-03-31'))
                 ->orderBy('step_num_all','desc')
                 ->get();
 
@@ -158,6 +160,8 @@ class ActivityController extends Core\Base
                     )  
                 ->groupBy('w_company_user.user_id')
                 ->where(['w_company_user.company_id' => $company_id,'w_company_user.department_id' => $department_id])
+                ->where('w_step_log.data_time','>=',strtotime('2018-03-01'))
+                ->where('w_step_log.data_time','<=',strtotime('2018-03-31'))
                 ->orderBy('step_num_all','desc')
                 ->get();
 
