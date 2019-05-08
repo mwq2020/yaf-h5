c92e8595af274a79c465c55aba38cc389d819a6d
mwq2020 <mwq2020@163.com>
2019-05-08 21:12:10 +0800

    fix

--- application/models/Step.php
+++ application/models/Step.php
@@ -257,7 +257,7 @@ class StepModel {
                 'w_department.name as department_name',
                 'w_department.department_id'
             )
-            ->where(['w_company_user.company_id' => $company_id,'w_company_user.is_tested' => 0,'w_company_user.status' => 1,'w_company_step_activity_user.status' => 1])
+            ->where(['w_company_user.company_id' => $company_id,'w_company_step_activity_user.activity_id'=>$activity_id,'w_company_user.is_tested' => 0,'w_company_user.status' => 1,'w_company_step_activity_user.status' => 1])
             ->groupBy('w_department.department_id')
             ->get();
         if(!empty($department_list)){
